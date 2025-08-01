e98374bacb5c4a11b28e5042b36a36b796a5efd7
WHWH1996 <545832239@qq.com>
2025-08-01 20:31:58 +0800

    update

--- web/cfpatron.php
+++ web/cfpatron.php
@@ -4,8 +4,54 @@
 ?>
 
 <h1>Sponsorship Benefits</h1>
-<p>For sponsors contributing USD 60,000 or more, APNet provides the following premier benefits package:</p>
-<h3>Brand Identity Integration</h3>
+
+<p>APNet offers different support package possibilities that can suit organizations of any size. These packages offer an opportunity for the supporting organization, to promote its visibility, services and products to a large community. It is also an opportunity to demonstrate support to research and development activities in the computer networking community.</p>
+
+<h2>Support Levels</h2>
+
+<ul>
+    <li>Bronze, USD$5,000</li>
+    <li>Silver, USD$10,000:</li>
+    <li>Gold, USD$20,000</li>
+    <li>Platinum, USD$60,000</li>
+</ul>
+
+<h2>Sponsorship Benefits</h2>
+    <p>All sponsors will be listed in the Conference Program, ACM Conference Proceedings and on the Conference Web site, with a link to the sponsor's company Home Page. Depending on the level of sponsorship, a sponsor will receive the following additional benefits</p>
+
+
+<h3>Bronze Sponsor</h3>
+    <ul>
+        <li>Display space for your company's promotional material at the conference</li>
+        <li>Logo on the conference's web site and online conference platform</li>
+        <li>Logo on official sponsorship signage at the conference</li>
+        <li>Logo on sponsor acknowledgement slide at the conference opening session</li>
+    </ul>
+
+<h3>Silver Sponsor</h3>
+    <ul>
+        <li>Display space for your company's promotional material at the conference</li>
+        <li>Logo on the conference's web site and online conference platform</li>
+        <li>A half-page listing on conference’s printed flyer (logo, URL and up to 150-word description) inserted in gift bag to registered attendees</li>
+        <li>Logo on official sponsorship signage at the conference</li>
+        <li>Logo on sponsor acknowledgement slide at the conference opening session</li>
+        <li>One full complimentary conference registration, including all lunches and dinners</li>
+    </ul>
+
+<h3>Gold Sponsor</h3>
+    <ul>
+        <li>Display space for your company's promotional material at the conference</li>
+        <li>Logo on the conference's web site and online conference platform</li>
+        <li>One full-page listing on conference’s printed flyer (logo, URL and up to 300-word description) inserted in gift bag to registered attendees</li>
+        <li>Additional (if any) company-provided promotion page or item to be given to all registered attendees</li>
+        <li>Logo on official sponsorship signage at the conference</li>
+        <li>Logo on sponsor acknowledgement slide at the conference opening session</li>
+        <li>Company-provided banner in the conference coffee break area</li>
+        <li>Three full complimentary conference registrations, including all lunches and dinners</li>
+    </ul>
+
+<h3>Platinum Sponsor</h3>
+ <p>1. Brand Identity Integration</p>
 <ul>
     <li>Sponsor's corporate logo will be listed on all digital platforms of the conference (official website, registration platform, etc.), with hyperlinks directing to Sponsor's corporate homepage.</li>
     <li>Sponsor's corporate logo will be displayed on all official sponsor recognition signage within the conference venue.</li>
@@ -14,14 +60,12 @@
     <li>Customized corporate display stands (content provided by Sponsor) will be placed in conference coffee break, lunch, and banquet areas.</li>
     <li>Sponsor's corporate promotional video (3-4 minutes duration) will be played on loop before the conference and during intermissions.</li>
 </ul>
-
-<h3>Physical Display Privileges</h3>
+<p>2. Physical Display Privileges</p>
 <ul>
     <li>A dedicated exhibition area will be established for Sponsor in the core section of the main conference venue, including a corporate display backdrop, three booths (with location priority for Sponsor), and three LCD screens for displaying promotional materials.</li>
     <li>A dedicated networking zone will be set up for Sponsor within the main conference venue to facilitate receptions and business meetings.</li>
 </ul>
-
-<h3>APNet Conference Privileges</h3>
+<p>3. APNet Conference Privileges</p>
 <ul>
     <li>Sponsor will receive up to six (inclusive) VIP fast-track passes for designated personnel (enterprise leaders above specified levels and experts), enabling priority access to main sessions and banquet events using special credential badges.</li>
     <li>Sponsor will receive twenty complimentary all-access conference registrations.</li>
